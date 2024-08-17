77a593f001d2466f84fa5fa0259c788364ec51de
Sir Nhlamulo Karabo <sir.nhlamulo@gmail.com>
2024-08-17 10:22:47 +0200

    element display

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,18 @@ let firstnameElem = mainForm.firstname,
   emailElem = mainForm.email,
   phonenoElem = mainForm.phoneno,
   summaryElem = mainForm.summary;
+
+// display elements
+let nameDsp = document.getElementById("fullname_dsp"),
+  imageDsp = document.getElementById("image_dsp"),
+  phonenoDsp = document.getElementById("phoneno_dsp"),
+  emailDsp = document.getElementById("email_dsp"),
+  addressDsp = document.getElementById("address_dsp"),
+  designationDsp = document.getElementById("designation_dsp"),
+  summaryDsp = document.getElementById("summary_dsp"),
+  projectsDsp = document.getElementById("projects_dsp"),
+  achievementsDsp = document.getElementById("achievements_dsp"),
+  skillsDsp = document.getElementById("skills_dsp"),
+  educationsDsp = document.getElementById("educations_dsp"),
+  experiencesDsp = document.getElementById("experiences_dsp");
+
