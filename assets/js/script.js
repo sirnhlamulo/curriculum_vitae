be125a92320d1435a2ea1da7e5dede74ec05b266
Sir Nhlamulo Karabo <sir.nhlamulo@gmail.com>
2024-08-17 10:19:13 +0200

    js input elemnts

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,10 @@ const emailRegex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const phoneRegex =
   /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-/* supports following number formats - (123) 456-7890, (123)456-7890, 123-456-7890, 123.456.7890, 1234567890, +31636363634, 075-63546725 */
+// supports following number formats - (123) 456-7890, (123)456-7890, 123-456-7890, 123.456.7890, 1234567890, +31636363634, 075-63546725
 const digitRegex = /^\d+$/;
 
+// data from view
 const mainForm = document.getElementById("cv-form");
 const validType = {
   TEXT: "text",
@@ -16,3 +17,14 @@ const validType = {
   PHONENO: "phoneno",
   ANY: "any",
 };
+
+// user inputs elements
+let firstnameElem = mainForm.firstname,
+  middlenameElem = mainForm.middlename,
+  lastnameElem = mainForm.lastname,
+  imageElem = mainForm.image,
+  designationElem = mainForm.designation,
+  addressElem = mainForm.address,
+  emailElem = mainForm.email,
+  phonenoElem = mainForm.phoneno,
+  summaryElem = mainForm.summary;
